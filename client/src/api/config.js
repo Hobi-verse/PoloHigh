882c57a218d22716466cbdc4df7a33cbe063d297
Aman Chaurasiya <chaurasiyaaman741@gmail.com>
2025-10-17 00:21:06 +0530

    cors fix

--- client/src/api/config.js
+++ client/src/api/config.js
@@ -1,4 +1,4 @@
-const DEFAULT_API_BASE_URL =import.meta.env.VITE_API_BASE_URL | "http://localhost:4000/api";
+const DEFAULT_API_BASE_URL = "http://localhost:4000/api";
 export const API_BASE_URL =
   typeof import.meta !== "undefined" && import.meta.env?.VITE_API_BASE_URL
     ? import.meta.env.VITE_API_BASE_URL
